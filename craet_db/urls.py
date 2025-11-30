4509cf48efc3f460ec2153fee4c625fe517c4ca1
oleg <olegpustovalov220@gmail.com>
2025-11-30 16:32:20 +0300

    swagger

--- craet_db/urls.py
+++ craet_db/urls.py
@@ -25,7 +25,30 @@ from db_crp.views_group import group_list, group_create, group_edit, group_delet
 from db_crp.views_user import user_list, user_create, user_info, user_edit, user_delete
 from db_crp.views_database import database_list, tables_list, database_connect, database_edit, database_delete, sync_users_and_groups
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
     path('admin/', admin.site.urls),  # Админка
 
     path('', home, name='home'),  # Главная
